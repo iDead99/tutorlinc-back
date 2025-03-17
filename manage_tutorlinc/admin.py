0765c0c330132acb8e678c873759b7dff1510961
idead <manyofrederick@gmail.com>
2025-03-17 13:01:03 +0000

    adding admin to check something

--- manage_tutorlinc/admin.py
+++ manage_tutorlinc/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from . models import Teacher
 
-# Register your models here.
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display=['user', 'phone', 'highest_qualification', 'availability_status', 'profile_picture']
+    list_per_page=10
+    list_select_related = ['user']
+    list_filter=['user']
+    ordering=['availability_status', 'user']
+    search_fields=['phone']
