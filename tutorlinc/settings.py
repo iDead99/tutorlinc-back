b0cba3c3ade978b04383e01bcafe76249b535109
idead <manyofrederick@gmail.com>
2025-04-02 13:44:27 +0000

    modifying the nax length of subject name and day to teach fields

--- tutorlinc/settings.py
+++ tutorlinc/settings.py
@@ -29,11 +29,11 @@ environ.Env.read_env()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 # ALLOWED_HOSTS = []
 
-# DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -123,10 +123,10 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
-# DATABASES = {
-#     'default': dj_database_url.parse(env('DATABASE_URL'))
-# }
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
