fc481b6e35ad26c54920c18dfbaa71abb8244530
idead <manyofrederick@gmail.com>
2025-04-14 04:59:17 +0000

    implementing password reset

--- tutorlinc/settings.py
+++ tutorlinc/settings.py
@@ -208,14 +208,14 @@ SIMPLE_JWT = {
 
 from decouple import config
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 465
-# EMAIL_USE_SSL = True
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-# EMAIL_TIMEOUT = 30
-# DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_TIMEOUT = 30
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 # SENDGRID
